980b3bd769244cbbe21631dbe0a305d822776706
ftruly <soulfxl@163.com>
2015-06-14 10:14:11 +0800

    TASK3

--- myCode.js
+++ myCode.js
@@ -1,2 +1,19 @@
+var height = 2.3;
 var arr = [5,6,1,3,2,9,8];
-console.log('array:',arr);
+
+var flag = true;//标识是否可以开始计数
+var sum = 0;
+
+for(var i=0;i<arr.length;i++){
+		//计数
+		if(arr[i]>height && flag==true){
+				sum++;
+				flag = false;
+		}
+		//设置标识位
+		if( arr[i] <= height && flag==false){
+				flag = true;
+		}
+}
+
+console.log('island number:',sum);
