01984ff680b3a3cc31d4ef798c4b7b77bdd9b564
ftruly <soulfxl@163.com>
2015-06-14 18:32:58 +0800

    TASK4

--- myCode.js
+++ myCode.js
@@ -1,19 +1,30 @@
-var height = 2.3;
 var arr = [5,6,1,3,2,9,8];
 
+var maxSum = 0;
 var flag = true;//标识是否可以开始计数
 var sum = 0;
 
+//循环获取高度
 for(var i=0;i<arr.length;i++){
+	//设置初始值
+	flag = true;//标识是否可以开始计数
+	sum = 0;
+
+	for(var j=0;j<arr.length;j++){
 		//计数
-		if(arr[i]>height && flag==true){
+		if(arr[j]>arr[i] && flag==true){
 				sum++;
 				flag = false;
 		}
 		//设置标识位
-		if( arr[i] <= height && flag==false){
+		if( arr[j] <= arr[i] && flag==false){
 				flag = true;
 		}
+	}
+
+	if(sum>maxSum){
+		maxSum = sum;
+	}
 }
 
-console.log('island number:',sum);
+console.log('island number:',maxSum);
